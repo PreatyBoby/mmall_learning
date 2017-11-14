3f4e44fcc3eaa526dd0f91f3a9ebfd8376901c79
PreatyBoby <404036704@qq.com>
2017-11-14 16:26:29 +0800

    add mmall project

---
+++ src/test/java/com/mmall/dao/UserMapperTest.java
@@ -0,0 +1,8 @@
+package com.mmall.dao;
+
+/**
+ * Created by Administrator on 2017/11/14.
+ */
+public class UserMapperTest {
+
+}
---
+++ src/test/java/com/mmall/service/UserServiceTest.java
@@ -0,0 +1,7 @@
+package com.mmall.service;
+
+/**
+ * Created by Administrator on 2017/11/14.
+ */
+public class UserServiceTest {
+}
